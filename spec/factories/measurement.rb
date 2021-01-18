7673016ea9c904eb075e6f7288769e23c1de29c2
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-18 07:38:20 +0100

    Add unit tests for controllers

--- spec/factories/measurement.rb
+++ spec/factories/measurement.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :measurement do
     value { Faker::Number.number(digits: 5) }
-    user_id { 25 }
-    measure_id { 12 }
+    user_id { Faker::Number.number(digits: 10) }
+    measure_id { Faker::Number.number(digits: 10) }
   end
 end
--- spec/requests/measurements_request_spec.rb
+++ spec/requests/measurements_request_spec.rb
@@ -1,5 +1,118 @@
 require 'rails_helper'
 
-RSpec.describe "Measurements", type: :request do
-  # TODO: Write tests for later for the whole requests folder
+RSpec.describe 'Measurements', type: :request do
+  # initialize test data
+  let(:user) { create(:user) }
+  let(:measure) { create(:measure) }
+  let!(:measurements) { create_list(:measurement, 10, user_id: user.id, measure_id: measure.id) }
+  let(:measurement_id) { measurements.first.id }
+  # authorize request
+  let(:headers) { valid_headers }
+
+  # Test suite for GET /measurements
+  describe 'GET /measurements' do
+    # make HTTP get request before each example
+    # update request with headers
+    before { get '/measurements', params: {}, headers: headers }
+
+    it 'returns measurements' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /measurements/:id
+  describe 'GET /measurements/:id' do
+    before { get "/measurements/#{measurement_id}", params: {}, headers: headers }
+
+    context 'when the record exists' do
+      it 'returns the measurement' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(measurement_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:measurement_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Measurement/)
+      end
+    end
+  end
+
+
+  # Test suite for POST /measurements
+  describe 'POST /measurements' do
+    # valid payload
+    let(:valid_attributes) do
+      # send json payload
+      { value: 23, user_id: user.id, measure_id: measure.id }.to_json
+    end
+
+    context 'when the request is valid' do
+      before { post '/measurements', params: valid_attributes, headers: headers }
+
+      it 'creates a measurement' do
+        expect(json['value']).to eq(23)
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      let(:invalid_attributes) { { value: nil }.to_json }
+      before { post '/measurements', params: invalid_attributes, headers: headers }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(json['message'])
+          .to match(/Validation failed: Measure must exist, Value can't be blank/)
+      end
+    end
+  end
+
+  # Test suite for PUT /measurements/:id
+  describe 'PUT /measurements/:id' do
+    let(:valid_attributes) { { value: 30 }.to_json }
+
+    context 'when the record exists' do
+      before { put "/measurements/#{measurement_id}", params: valid_attributes, headers: headers }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  # Test suite for DELETE /measurements/:id
+  describe 'DELETE /measurements/:id' do
+    before { delete "/measurements/#{measurement_id}", params: {}, headers: headers }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
