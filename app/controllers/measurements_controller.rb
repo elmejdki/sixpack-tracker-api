61f384f180945753623064c07fb69522ac52a0a4
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-02-01 16:09:00 +0100

    Update measurements controller
    This commit will:
     - Remove unnecessary actions(show, update, destroy)
     - Update measurement_params to accept an array instead of object
     - Update the create action to be able to save a collection of data
     instead of one record
     - order measurements on the index action

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,49 +1,21 @@
 class MeasurementsController < ApplicationController
-  before_action :set_measure, only: %i[show update destroy]
-
   # GET /measurements
   def index
-    @measurements = current_user.measurements.all
-
+    @measurements = current_user.measurements.all.order(created_at: :DESC)
     json_response(@measurements)
   end
 
   # POST /measurements
   def create
-    @measurement = current_user.measurements.create!(measurement_params)
-    json_response(@measurement, :created)
-  end
-
-  # GET /measurements/:id
-  def show
-    json_response(@measurement)
-  end
-
-  # PUT /measurements/:id
-  def update
-    @measurement.update(measurement_params)
-    if measurement_params[:image] && @measurement.image.attached?
-      @measurement.update(image_url: url_for(@measurement.image))
-    end
-    head :no_content
-  end
-
-  # DELETE /measurements/:id
-  def destroy
-    @measurement.destroy
-    head :no_content
+    @measurements = current_user.measurements.create!(measurement_params[:measurements])
+    json_response(@measurements, :created)
   end
 
   private
 
   def measurement_params
     params.permit(
-      :value,
-      :measure_id
+      measurements: [:measure_id, :value, :created_at]
     )
-  end
-
-  def set_measure
-    @measurement = current_user.measurements.find(params[:id])
   end
 end
